44e06f4868b64d29b7a3b9b2dd7022c7b7286f7e
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-25 15:47:14 +0000

    iffi

---
+++ .devcontainer/03_basics/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expression(IIFE)
+
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();
+
+
+// ( (name) => {
+//     console.log(`${name} DB CONNECTED`);    // arrow function
+// })('Linku');
+
