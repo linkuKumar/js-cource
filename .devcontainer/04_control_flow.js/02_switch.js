06b2c1148222dbc4a704dfe83841674709d21ebb
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-26 15:38:21 +0000

    switch cases

---
+++ .devcontainer/04_control_flow.js/02_switch.js
@@ -0,0 +1,42 @@
+//switch cases 
+
+// const month= 3
+
+// switch (month) {
+//     case 1:
+//         console.log('january');
+//         break;
+//     case 2:
+//         console.log('february');
+//         break;
+//     case 3:
+//         console.log('march');
+//         break;
+//     case 4:
+//         console.log('april');
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month= 'april'
+
+// switch (month) {
+//     case 'january':
+//         console.log('january');
+//         break;
+//     case 'february':
+//         console.log('february');
+//         break;
+//     case 'march':
+//         console.log('march');
+//         break;
+//     case 'april':
+//         console.log('april');
+//         break;
+
+//     default:
+//         break;
+// }
