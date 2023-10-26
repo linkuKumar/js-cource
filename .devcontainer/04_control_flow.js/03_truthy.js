bb7d00f922a34fa54ac16d53ec73f704682af9a1
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-26 15:56:23 +0000

    logical flow

---
+++ .devcontainer/04_control_flow.js/03_truthy.js
@@ -0,0 +1,38 @@
+// const userEmail ="linku@gmail.com"
+
+// if(userEmail){
+//     console.log('got user email');
+// }else{
+//     console.log("dont have user email");
+// }
+
+
+
+//falsy value 
+// false / 0 /-0 / BigInt / 0n / ''/ null/ undefined/NaN  these values are considered as falsy values while validation
+
+//Truth values
+//'0' /'false' / ' ' / [] / {} /function(){} / 
+
+// if (userEmail.length ===0){
+//     console.log("this is the correct way to validate the array");
+// }
+
+// const emptyObj ={}
+// if(Object.keys(emptyObj).length ===0){
+//     console.log('this is the correct way to handle or validate  an array');
+// }
+
+
+//nullish coalescing Operators (??): null undefined
+
+// let val1;
+// // val1=5 ?? 10
+// // val1 =null ?? 10
+// // val1 = undefined ?? 15
+// // val1 = undefined ?? 15 ?? 10  
+
+// console.log(val1);
+
+// const iceTeaPrice =100
+// iceTeaPrice >= 80 ? console.log('less than 80') : console.log('price is more than 80');
