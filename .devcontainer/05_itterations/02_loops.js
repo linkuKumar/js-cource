025f2f0a99156d4d711d5f7c87a1734d5eeb2cbb
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-26 16:44:07 +0000

    do while loops

---
+++ .devcontainer/05_itterations/02_loops.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let index=0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let index= ['Linku','Kumar','Gauda']
+// let i = 0
+// while (i < index.length) {
+//     console.log(`value of index is ${index[i]}`);
+//     i = i+1;
+// }
+
+
+
+// do while
+
+let score = 1
+do {
+    console.log(`the value of score is ${score}`);
+    score++
+} while (score <= 10);
