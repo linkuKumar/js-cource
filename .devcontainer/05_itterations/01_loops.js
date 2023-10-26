58eff22493542630b2020eddac18c47067b00684
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-26 16:20:27 +0000

    loops

---
+++ .devcontainer/05_itterations/01_loops.js
@@ -0,0 +1,49 @@
+//for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log('5 is the best number');
+//     }
+//     console.log(element);
+// }
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log('outer loop');
+//    for (let j = 0; j <= 10; j++) {
+//     console.log('inner  loop' , i*j);
+//    } 
+// }
+
+
+// let myArray = ['linku','kumar','gauda',]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+//use of break and continue
+
+// for (let i = 0; i <= 20; i++) {
+//     if(i ==5){
+//         console.log('5 detected')
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+
+// for (let i = 0; i <= 20; i++) {
+//     if(i ==5){
+//         console.log('5 detected')
+//         continue
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
