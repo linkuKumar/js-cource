1cc3b0d3865b72b2adc4568050793e4015588c76
linkuKumar <148623676+linkuKumar@users.noreply.github.com>
2023-10-27 11:59:22 +0000

    for in loops

---
+++ .devcontainer/05_itterations/04_loops.js
@@ -0,0 +1,20 @@
+// const myObject ={
+//     js:'javascript',
+//     cpp:'c++',
+//     rb:'ruby',
+//     swift:'swift by apple'
+// }
+// // tested for arrays 
+// for (const key in myObject) {
+//     console.log(`${key} is the key for  ${myObject[key]}`);
+// }
+
+
+
+//tesing for in for arrays 
+// const programming = ['js','rb','py','java','cpp']
+
+// for (const key in programming) {
+//   console.log(programming[key]);
+// }
+
